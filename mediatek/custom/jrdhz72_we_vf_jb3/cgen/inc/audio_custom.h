473bb0dc3064df91876a0e8c1bfbb99748e04940
abgoyal <abgoyal@gmail.com>
2015-02-08 19:16:10 +0530

    Initial git commit from Alcatel Source repo @ http://sourceforge.net/projects/alcatel

---
+++ mediatek/custom/jrdhz72_we_vf_jb3/cgen/inc/audio_custom.h
@@ -0,0 +1,10 @@
+/*********************************************************
+History:
+2013-0607 modify for YarisM_Proto_Audio Parameters_V0.5 by yi.zheng.hz
+*********************************************************/
+#ifndef MTK_2IN1_SPK_SUPPORT
+#include "EU/audio_custom.h"
+#else
+#include "US/audio_custom.h"
+#endif
+
---
+++ mediatek/custom/jrdhz72_we_vf_jb3/preloader/inc/custom_MemoryDevice.h
@@ -0,0 +1,24 @@
+
+
+#ifndef __CUSTOM_MEMORYDEVICE__
+#define __CUSTOM_MEMORYDEVICE__
+
+
+
+#define BOARD_ID                MT6572_EVB1
+
+#define CS_PART_NUMBER[0]       KMN5U000ZM_B203
+/*add by yinzhanyong for fr437054 at 20130422 begin*/
+#define CS_PART_NUMBER[1]       MT29PZZZ4D4WKETF_18_W_6E4
+/*add by yinzhanyong for fr437054 at 20130422 end*/
+/*add by yinzhanyong for fr483691 at 20130704 begin*/
+#define CS_PART_NUMBER[2]       KMN5W000ZM_B207
+/*add by yinzhanyong for fr483691 at 20130704 end*/
+/*add by csli for fr510032 at 20130821 begin*/
+#define CS_PART_NUMBER[3]       TYC0FH121597RA
+/*add by csli for fr510032 at 20130821 end */
+#define CS_PART_NUMBER[4]       KMN5X000ZM_B209
+
+#define EMI_CLK                 266M
+
+#endif /* __CUSTOM_MEMORYDEVICE__ */
